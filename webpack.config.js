799d6952758d17ed04bda97cd4b7c00d8f0fb1f3
buchslava <vyacheslav.chub@gmail.com>
2020-12-28 00:44:00 +0200

    fix webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,23 +1,40 @@
-var path = require("path");
-
 module.exports = {
-  mode: "production",
-  entry: "./index.jsx",
-  output: {
-    path: path.resolve("lib"),
-    filename: "index.js",
-    libraryTarget: "commonjs2",
-  },
-  optimization: {
-    minimize: false,
-  },
+  entry: [
+    './index.jsx'
+  ],
   module: {
     rules: [
       {
-        test: /\.jsx?$/,
-        exclude: /(node_modules)/,
-        use: "babel-loader",
-      },
-    ],
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: ['babel-loader']
+      }
+    ]
   },
+  externals: {
+    react: {
+        root: 'React',
+        commonjs2: 'react',
+        commonjs: 'react',
+        amd: 'react'
+    },
+    'react-dom': {
+        root: 'ReactDOM',
+        commonjs2: 'react-dom',
+        commonjs: 'react-dom',
+        amd: 'react-dom'
+    }
+  },
+  resolve: {
+    extensions: ['*', '.js', '.jsx']
+  },
+  output: {
+    path: __dirname + '/lib',
+    publicPath: '/',
+    filename: 'index.js',
+    libraryTarget: 'commonjs2'
+  },
+  devServer: {
+    contentBase: './lib'
+  }
 };
